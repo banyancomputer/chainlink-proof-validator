92e7c23bb0d28766e7483e3058846b5ee4976479
Jonah Kaye <jonahkaye@Jonahs-MacBook-Air.local>
2022-08-22 20:04:41 +0300

    trigger working

--- hardhat_test/scripts/trigger.js
+++ hardhat_test/scripts/trigger.js
@@ -5,16 +5,28 @@ const {TextEncoder} = require("util");
 const { exit } = require("node:process");
 
 async function main() {
-
-  let txtFile = "bao_slice_2.txt";
-  let proof = fs.readFileSync(txtFile);
   
   await hre.run("compile")
-  const Bao = await ethers.getContractFactory("Bao");
-  const bao = await Bao.attach("0x464cBd3d0D8A2872cf04306c133118Beb5711111"); // WRONG 
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.attach("0x464cBd3d0D8A2872cf04306c133118Beb5711111"); // WRONG 
 
-  const transactionResponse = await bao.save_proof(proof);
-  const transactionReceipt = await transactionResponse.wait()
-  console.log(transactionReceipt)
-  console.log(transactionReceipt.events[0].args)
+  let offerId = 55378008;
+  let deal_start_block = 2; 
+  let deal_length_in_blocks = 3; 
+  let proof_frequency_in_blocks = 4; 
+  let price = 5; 
+  let collateral = 6; 
+  let erc20_token_denomination = '0xf679d8d8a90f66b4d8d9bf4f2697d53279f42bea'; // addr 
+  let ipfs_file_cid = 8; 
+  let file_size = 9; 
+  let blake3_checksum = 10; 
+
+  const transactionResponse_2 = await lock.createOffer(offerId, deal_start_block, deal_length_in_blocks, proof_frequency_in_blocks, price, collateral, erc20_token_denomination, ipfs_file_cid, file_size, blake3_checksum);
+  const transactionReceipt_2 = await transactionResponse_2.wait(); 
+  console.log(transactionReceipt_2)
 }
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
