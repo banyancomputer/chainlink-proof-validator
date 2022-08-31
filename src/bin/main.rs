788bc1159c8a4f5770e1e50db18afb72063b3cd0
Zev Kent <zkent@uchicago.edu>
2022-08-30 20:13:08 -0700

    testing scripts progress, example computation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,9 +7,32 @@ extern crate serde_json;
 extern crate rust_chainlink_ea_api;
 
 use rust_chainlink_ea_api::validate;
-use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize, json::Json};
 use eyre::Result;
 use validate::{ChainlinkRequest, MyResult};
+use ethers::providers::{Provider, Middleware, Http};
+use std::time::Duration;
+
+/* Example data structures */
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ExampleRequestData {
+    block_num: u64
+}
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ExampleRequest {
+    id: String,
+    data: ExampleRequestData
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ExampleResponse {
+    data: Duration,
+    status: u16,
+    result: String
+}
 
 // check about timeouts with chainlink 
 
@@ -22,11 +45,74 @@ async fn val(input_data: Json<ChainlinkRequest>) -> Json<MyResult> {
 
 }
 
+fn construct_error(reason: String) -> Json<ExampleResponse> {
+    Json(ExampleResponse {
+        data: Duration::new(0, 0),
+        status: 500,
+        result: reason
+    })
+}
+
+#[post("/compute", format = "json", data = "<input_data>")]
+async fn compute(input_data: Json<ExampleRequest>) -> Json<ExampleResponse> {
+
+    let block_num = input_data.into_inner().data.block_num;
+
+    let provider = Provider::<Http>::try_from(
+        "https://goerli.infura.io/v3/1a39a4b49b9f4b8ba1338cd2064fe8fe")
+        .expect("could not instantiate HTTP Provider");
+
+    let current_block_num = match provider.get_block_number().await {
+        Ok(n) => n.as_u64(),
+        Err(e) => 
+            return construct_error(format!("Could not get block number: {e}"))
+    };
+    println!("current block num: {}", current_block_num);
+    if block_num < 0 || block_num > current_block_num {
+        return construct_error(format!("Block number {block_num} is invalid."))
+    }
+
+    let current_block = match provider.get_block(current_block_num).await {
+        Ok(b) => b,
+        Err(e) => 
+            return construct_error(format!("Could not get block number {}: {}", current_block_num, e))
+    };
+    let current_block_time = match current_block.unwrap().time() {
+        Ok(dt) => dt,
+        Err(e) => return construct_error(format!("{e}"))
+    };
+    let target_block = match provider.get_block(block_num).await {
+        Ok(b) => b,
+        Err(e) => 
+            return construct_error(format!("Could not get block number {}: {}", block_num, e))
+    };
+    let target_block_time = match target_block.unwrap().time() {
+        Ok(dt) => dt,
+        Err(e) => return construct_error(format!("{e}"))
+    };
+    println!("target block time: {}", target_block_time);
+    println!("current block time: {}", current_block_time);
+    let diff = match current_block_time.signed_duration_since(target_block_time).to_std() {
+        Ok(d) => d,
+        Err(e) => 
+            return construct_error(format!("Could not convert time to standard: {e}"))
+    };
+    println!("difference: {:?}", diff);
+    let status = 200;
+    let result = "Ok".to_string();
+
+    Json(ExampleResponse {
+        data: diff,
+        status: status,
+        result: result
+    })
+}
+
 #[rocket::main]
 async fn main() -> Result<()> {
 
     let _rocket = rocket::build()
-        .mount("/", routes![val])
+        .mount("/", routes![val, compute])
         .launch()
         .await?;
 
--- src/bin/make_test.rs
+++ src/bin/make_test.rs
@@ -1,13 +1,21 @@
-use std::fs::File;
-use std::io::Read;
+use core::slice;
+use std::{fs::{File, read, copy},
+          io::{Read, Write}};
 use ethers::{types::H256,
              providers::{Middleware, Provider, Http}};
-use anyhow;
+use anyhow::Error;
+use tempfile;
 
-extern crate rust_chainlink_ea_api;
-use rust_chainlink_ea_api::types::BlockNum;
+use banyan_shared::{types::*, proofs};
 
-pub async fn compute_target_block_hash(target_window_start: BlockNum) -> Result<H256, anyhow::Error> {
+#[derive(PartialEq)]
+pub enum Quality {
+    Good,
+    Bad
+}
+
+/* Computes the target block hash from the target block number */
+pub async fn compute_target_block_hash(target_window_start: BlockNum) -> Result<H256, Error> {
     let provider = Provider::<Http>::try_from(
         "https://goerli.infura.io/v3/1a39a4b49b9f4b8ba1338cd2064fe8fe")
         .expect("could not instantiate HTTP Provider");
@@ -19,6 +27,8 @@ pub async fn compute_target_block_hash(target_window_start: BlockNum) -> Result<
     Ok(target_block_hash)
 }
 
+/* Reads a local text file from a directory called "files/" and finds the 
+   length of the file */
 pub fn file_len(file_name: &str) -> usize {
     let mut file_content = Vec::new();
     let dir = "files/";
@@ -29,13 +39,65 @@ pub fn file_len(file_name: &str) -> usize {
     return length;
 }
 
+pub async fn create_proof_helper(target_window_start: BlockNum, file: &str, quality: Quality) -> Result<(bao::Hash, u64, Vec<u8>), Error> {
+    std::fs::create_dir_all("proofs/")?;
+    let target_block_hash = compute_target_block_hash(target_window_start).await?;
+    let split = file.split(".").collect::<Vec<&str>>();
+    let input_file_name = split[0];
+    let file_length = file_len(input_file_name) as u64;
+    let (chunk_offset, chunk_size) = 
+        proofs::compute_random_block_choice_from_hash(target_block_hash, file_length);
+    
+    let f = File::open(file)?;
+    let (hash, mut bao_file) = proofs::gen_obao(f).await?;
+    let mut slice_input_file = tempfile::tempfile()?;
+    if quality == Quality::Bad {
+        let mut bytes: Vec<u8> = Vec::new();
+        bao_file.read_to_end(&mut bytes)?;
+        let last_index = bytes.len() - 1;
+        bytes[last_index] ^= 1;
+        slice_input_file.write(&bytes)?;
+    }
+    else {
+        slice_input_file = bao_file;
+    }
+    
+    let mut extractor = 
+        bao::encode::SliceExtractor::new(slice_input_file, 
+                                        chunk_offset, 
+                                        chunk_size);
+    let mut slice = Vec::new();
+    extractor.read_to_end(&mut slice)?;
 
+    Ok((hash, file_length, slice))
+}
+
+pub async fn create_good_proof(target_window_start: BlockNum, file: &str) -> Result<(bao::Hash, u64, Vec<u8>), Error> {
+    create_proof_helper(target_window_start, file, Quality::Good).await
+}
+
+pub async fn create_bad_proof(target_window_start: BlockNum, file: &str) -> Result<(bao::Hash, u64, Vec<u8>), Error> {
+    create_proof_helper(target_window_start, file, Quality::Bad).await
+}
 
 fn main() {
-    let file_name = "ethereum.pdf";
+    let file_name = "files/filecoin.pdf";
     let mut file_content = Vec::new();
     let mut file = File::open(&file_name).expect("Unable to open file");
     file.read_to_end(&mut file_content).expect("Unable to read");
     let length = file_content.len();
     println!("file length: {}", length);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_file_len() {
+        let eth_len = file_len("ethereum.pdf");
+        let filecoin_len = file_len("filecoin.pdf");
+        assert_eq!(eth_len, 941366);
+        assert_eq!(filecoin_len, 629050);
+        //assert_eq!(File::open("files/ethereum.pdf")?.metadata().unwrap().len(), 941366);
+    }
 }
