4293968add12d014215517b965aec420e1ba6a24
jonahkaye <jonah@banyan.computer>
2022-09-15 22:48:36 +0300

    TODOs finished and a lot cleaner

--- src/validate.rs
+++ src/validate.rs
@@ -32,7 +32,6 @@ cargo build --release for benchmarking
 */
 use anyhow::Result;
 use banyan_shared::{eth::VitalikProvider, proofs, proofs::window, types::*};
-use ethers::{types::{Filter, H256}, utils::get_contract_address};
 use log::info;
 use rocket::{
     post,
@@ -41,8 +40,6 @@ use rocket::{
 };
 use std::io::{Cursor, Read};
 use std::sync::Arc;
-
-const WORD: usize = 32;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Status {
@@ -74,7 +71,6 @@ pub struct MyResult {
     pub status: Status,
     pub result: String,
 }
-
 pub struct WebserverState(pub Arc<VitalikProvider>);
 
 /* Function to construct an error response to return to Chainlink */
@@ -136,19 +132,17 @@ async fn validate_deal_internal(
     // iterating over proof blocks (by window)
     let mut success_count = 0;
     for window_num in 0..num_windows {
-        
         let target_window_start = VitalikProvider::compute_target_window_start(
             deal_info.deal_start_block,
             deal_info.proof_frequency_in_blocks,
-            window_num); 
+            window_num,
+        );
 
         let target_block_hash = provider
             .get_block_hash_from_num(target_window_start)
             .await
             .map_err(|e| format!("Could not get block hash: {e}"))?;
 
-        // step b. above
-        // TODO: perhaps this just means they didn't submit? shouldn't just error here.
         let submitted_proof_in_block_num = match provider
             .get_proof_block_num_from_window(deal_id, window_num as u64)
             .await
@@ -162,73 +156,43 @@ async fn validate_deal_internal(
             }
         };
 
-        // TODO these should be rolled up into one function in eth_shared pls
-        let filter = Filter::new()
-            .select(submitted_proof_in_block_num.0)
-            // TODO figure this guy out later :)
-            .address(provider.get_contract_address())
-            .topic1(H256::from_low_u64_be(deal_id.0));
+        let proof_bytes: Vec<u8> = match provider
+            .get_proof_from_logs(submitted_proof_in_block_num, deal_id)
+            .await
+            .map_err(|e| {
+                format!(
+                    "Couldn't get log from block {}: {}",
+                    submitted_proof_in_block_num.0, e
+                )
+            })? {
+            Some(proof) => proof,
+            None => {
+                info!("Proof is too short for window {}", window_num);
+                continue;
+            }
+        };
 
-        let block_logs = provider.get_logs_from_filter(&filter).await.map_err(|e| {
-            format!(
-                "Couldn't get log from block {}: {}",
-                submitted_proof_in_block_num.0, e
-            )
-        })?;
-
-        // The first two 32 byte words of log data are a pointer and the size of the data.
-        // TODO put this in banyan_shared!
-        let data = &block_logs[0].data;
-        if data.len() < WORD * 2 {
-            return Err(format!("Data is too short: {:?}", data));
-        }
-
-        // TODO yeah there's definitely a bug here. this slice is only 32 bytes long, not 64.
-        // TODO This works probably as a solution but might be a bug. I could submit a really long block, 
-        // but since only takes last 8 bytes, it could slice the length value such that it read the
-        // length as the actual size. That would cause it to only read the first x bytes of the proof,
-        // and then it could read an incorrect proof as correct. BUTT the proof would have to have to 
-        // be incorrect in such a way that it had the correct proof in the first x bytes, and then other 
-        // incorrect stuff after, which means someone would have had to construct an correct proof and then
-        // decide to append other stuff for some reason. I don't know how that would help an attacker. 
-        // Is this a bug? I am not sure. 
-
-        let mut a = [0u8; 8];
-            a.clone_from_slice(&data[(WORD * 2) - 8..WORD * 2]);
-            println!("a: {:?}", a);
-        let data_size = u64::from_be_bytes({
-            //let mut a = [0u8; 8];
-            //a.clone_from_slice(&data[WORD..WORD + 8]);
-            a
-        });
-        println!("data size: {}", data_size);
-        if data.len() < WORD * 2 + data_size as usize {
-            return Err(format!("Proof is too short: {:?}", data));
-        }
-
-        let data_bytes = &data[WORD * 2..WORD * 2 + data_size as usize];
-        let proof_bytes = Cursor::new(data_bytes);
-
-        // step e. above
         let (chunk_offset, chunk_size) =
             proofs::compute_random_block_choice_from_hash(target_block_hash, deal_info.file_size);
 
-        println!("target window: {}", target_window_start.0);
-        println!("chunk offset val: {}", chunk_offset);
-        // step f. above
-        // TODO put this in banyan shared. 
-        if bao::decode::SliceDecoder::new(
-            proof_bytes,
-            &(deal_info.blake3_checksum),
+        // TODO is there an issue of coercing the Vec<u8> into a &[u8] here?
+        match VitalikProvider::check_if_merkle_proof_is_valid(
+            Cursor::new(&proof_bytes),
+            deal_info.blake3_checksum,
             chunk_offset,
             chunk_size,
         )
-        .read_to_end(&mut vec![])
-        .is_ok()
-        {
-            success_count += 1
-        } else {
-            info!("Proof failed for window {}", window_num);
+        .map_err(|e| {
+            format!(
+                "Error reading proof {}: {}",
+                submitted_proof_in_block_num.0, e
+            )
+        })? {
+            true => success_count += 1,
+            false => {
+                info!("Proof failed for window {}", window_num);
+                continue;
+            }
         }
     }
     if num_windows > 0 {
